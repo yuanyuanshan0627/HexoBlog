e6e717135bba3e37c6d07048aa428edf0fee3250
yuanyuanshan0627 <119478320+yuanyuanshan0627@users.noreply.github.com>
2023-08-31 19:17:45 +0800

    Update fomal.js

--- source/js/fomal.js
+++ source/js/fomal.js
@@ -3174,7 +3174,7 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
+    --default-bg: url(https://fastly.picsum.photos/id/96/1920/1080.webp?hmac=jcaOPTpFtbX-bnkFj8CnjQbujBYxFfwfPqH-IOAiuTM);
     --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
     --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
     --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
